57c6f89077c339ddc9058521ee9cf156ab4d0b9c
Taimoor Hasan Khan <94681976+thkodin@users.noreply.github.com>
2023-11-14 04:07:34 +0500

    import max_length for charfields from constants.py

--- encoder/models.py
+++ encoder/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
+from .constants import MAX_MESSAGE_LENGTH
+
 
 # Create your models here.
 class UserMessage(models.Model):
     """Message model."""
 
-    message = models.CharField(max_length=120, blank=False, null=False)
+    message = models.CharField(max_length=MAX_MESSAGE_LENGTH, blank=False, null=False)
